96766b71606d8831e976f53b5920ac772324a1f9
Lowraince <neson@yahoo.com>
2025-04-27 19:32:05 +0300

    feat: add eslint

---
+++ eCommerce-Application/eslint.config.mjs
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    rules: {
+      "@typescript-eslint/consistent-type-assertions": [
+        "error",
+        { assertionStyle: "never" },
+      ],
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        { accessibility: "explicit", overrides: { constructors: "off" } },
+      ],
+      "@typescript-eslint/member-ordering": "error",
+      "class-methods-use-this": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "max-lines-per-function": ["error", 40],
+    },
+  },
+  {
+    ignores: ["*.spec.ts", "dist/", "node_modules"],
+  },
+  tseslint.configs.recommended,
+]);
