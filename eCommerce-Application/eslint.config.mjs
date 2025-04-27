e657902c86b9ae4a5a8845791c38cc03c056f99f
Lowraince <neson@yahoo.com>
2025-04-27 23:30:03 +0300

    feat: add new rule for unicorn

--- eCommerce-Application/eslint.config.mjs
+++ eCommerce-Application/eslint.config.mjs
@@ -39,4 +39,9 @@ export default defineConfig([
   tseslint.configs.recommended,
   eslintConfigPrettier,
   eslintPluginUnicorn.configs.recommended,
+  {
+    rules: {
+      "unicorn/prefer-top-level-await": "off",
+    },
+  },
 ]);
