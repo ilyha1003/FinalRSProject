0b8e1843400bddd01889d61396cd8cb67abbab39
Sankovich Ilya <ilyhasankovich@gmail.com>
2025-04-29 19:43:10 +0300

    chore: add base folder structure

---
+++ eCommerce-Application/src/app/pages/basket-page/basket-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basket-page',
+  imports: [],
+  templateUrl: './basket-page.component.html',
+  styleUrl: './basket-page.component.scss'
+})
+export class BasketPageComponent {
+
+}
