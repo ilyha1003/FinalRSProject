2f75b19945090c4db98eda51f33a67e3850b3b81
mavlekiev <mavlekiev@gmail.com>
2025-05-12 20:06:51 +0300

    fix: fix header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import type { Routes } from '@angular/router';
 import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { HeaderComponent } from './components/header/header.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 export const routes: Routes = [
-  { path: '', component: HeaderComponent },
+  { path: '', component: MainPageComponent },
   { path: 'registration', component: RegistrationPageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: '**', component: NotFoundComponent },
