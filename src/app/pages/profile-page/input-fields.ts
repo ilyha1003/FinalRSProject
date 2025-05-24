6c5826c5a03c06b00837b8ccdb5a49f02063445b
Sankovich Ilya <ilyhasankovich@gmail.com>
2025-05-25 01:55:33 +0300

    feat: add shipping and billing fields in customers profile and change the design of inputs

--- src/app/pages/profile-page/input-fields.ts
+++ src/app/pages/profile-page/input-fields.ts
@@ -1,17 +1,89 @@
-export const inputFields = [
+export const generalInputFields = [
   {
     label: 'Email address',
     type: 'email',
     controlName: 'email',
+    id: 'emailInput',
+    track: 'email',
+    value: '',
   },
   {
     label: 'First name',
     type: 'text',
     controlName: 'firstName',
+    id: 'firstNameInput',
+    track: 'firstName',
+    value: '',
   },
   {
     label: 'Last name',
     type: 'text',
     controlName: 'lastName',
+    id: 'lastNameInput',
+    track: 'lastName',
+    value: '',
+  },
+];
+
+export const generalInputFieldBirthDate = {
+  label: 'Birth date',
+  type: 'date',
+  controlName: 'birthDate',
+  id: 'birthDate',
+  track: 'birthDate',
+  value: '',
+};
+
+export const shippingAddresses = [
+  {
+    label: 'City',
+    type: 'text',
+    id: 'shippingCity',
+    controlName: 'shippingCity',
+    track: 'shippingCity',
+    value: '',
+  },
+  {
+    label: 'Postal code',
+    type: 'text',
+    id: 'shippingPostalCode',
+    controlName: 'shippingPostalCode',
+    track: 'shippingPostalCode',
+    value: '',
+  },
+  {
+    label: 'Address',
+    type: 'text',
+    id: 'shippingAddress',
+    controlName: 'shippingAddress',
+    track: 'shippingAddress',
+    value: '',
+  },
+];
+
+export const billingAddresses = [
+  {
+    label: 'City',
+    type: 'text',
+    id: 'billingCity',
+    controlName: 'billingCity',
+    track: 'billingCity',
+    value: '',
+  },
+  {
+    label: 'Postal code',
+    type: 'text',
+    id: 'billingPostalCode',
+    controlName: 'billingPostalCode',
+    track: 'billingPostalCode',
+    value: '',
+  },
+  {
+    label: 'Address',
+    type: 'text',
+    id: 'billingAddress',
+    controlName: 'billingAddress',
+    track: 'billingAddress',
+    value: '',
   },
 ];
--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,6 +1,12 @@
 import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { inputFields } from './input-fields';
+import { countries } from '../../utils/countries';
+import {
+  generalInputFields,
+  generalInputFieldBirthDate,
+  shippingAddresses,
+  billingAddresses,
+} from './input-fields';
 import {
   FormControl,
   FormGroup,
@@ -10,10 +16,13 @@ import {
 import { customEmailValidator } from '../../utils/validations/email-custom-validator';
 import { birthDateValidator } from '../../utils/validations/birth-date-validator';
 import { hasError } from '../../utils/validations/has-error';
+import { postalCodeValidator } from '../../utils/validations/postal-code-validator';
+import { noSpacesValidator } from '../../utils/validations/no-spaces-validator';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [NgIf, NgClass, ReactiveFormsModule],
+  imports: [NgIf, NgClass, ReactiveFormsModule, FormsModule],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss',
 })
@@ -37,8 +46,51 @@ export class ProfilePageComponent {
     birthDate: new FormControl('', [Validators.required, birthDateValidator]),
   });
 
-  public activeMainButton = 'general';
-  public inputFields = inputFields;
+  public shippingProfileForm = new FormGroup({
+    shippingCountry: new FormControl('', [Validators.required]),
+    shippingCity: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[A-Za-zА-Яа-яЁё]+$'),
+    ]),
+    shippingPostalCode: new FormControl('', [
+      Validators.required,
+      postalCodeValidator,
+      noSpacesValidator,
+    ]),
+    shippingAddress: new FormControl('', [
+      Validators.required,
+      noSpacesValidator,
+    ]),
+  });
+
+  public billingProfileForm = new FormGroup({
+    billingCountry: new FormControl('', [Validators.required]),
+    billingCity: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[A-Za-zА-Яа-яЁё]+$'),
+    ]),
+    billingPostalCode: new FormControl('', [
+      Validators.required,
+      postalCodeValidator,
+      noSpacesValidator,
+    ]),
+    billingAddress: new FormControl('', [
+      Validators.required,
+      noSpacesValidator,
+    ]),
+  });
+
+  public activeMainButton: string = 'general';
+  public isEditMode: boolean = false;
+  public isShippingDefaultChecked: boolean = false;
+
+  public generalInputFields = generalInputFields;
+  public generalInputFieldBirthDate = generalInputFieldBirthDate;
+  public shippingAddress = shippingAddresses;
+  public billingAddress = billingAddresses;
+  public countries = countries;
+
+  public isFocused: Record<string, boolean> = {};
   public hasError = hasError;
 
   public get passwordErrorCount(): number {
@@ -48,5 +100,20 @@ export class ProfilePageComponent {
 
   public setActiveMainButton(buttonName: string): void {
     this.activeMainButton = buttonName;
+  }
+
+  public setActiveEditMode(): void {
+    this.isEditMode = true;
+  }
+
+  public setInactiveEditMode(): void {
+    this.isEditMode = false;
+  }
+
+  public onCheckboxChange(event: Event): void {
+    const target = event.target;
+    if (target instanceof HTMLInputElement) {
+      this.isShippingDefaultChecked = target.checked;
+    }
   }
 }
