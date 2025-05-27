93c7628a5214f3709f803a457863e68a910f8b08
Sankovich Ilya <ilyhasankovich@gmail.com>
2025-05-27 03:01:25 +0300

    feat: add integration of ecommerce with general field of customer profile page

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -22,6 +22,9 @@ import { postalCodeValidator } from '../../utils/validations/postal-code-validat
 import { noSpacesValidator } from '../../utils/validations/no-spaces-validator';
 import { FormsModule } from '@angular/forms';
 import { strengthPasswordValidator } from '../../utils/validations/strength-password-validator';
+// import { ApiService } from '../../services/api.service';
+import { LocalStorageService } from '../../services/local-storage.service';
+import { ProfileService } from '../../services/profile.service';
 
 @Component({
   selector: 'app-profile-page',
@@ -48,6 +51,13 @@ export class ProfilePageComponent {
     ]),
     birthDate: new FormControl('', [Validators.required, birthDateValidator]),
   });
+
+  public generalProfileFormFields = {
+    email: 'test@mail.ru',
+    firstName: 'Ilya',
+    lastName: 'Sankovich',
+    birthDate: '1999-03-10',
+  };
 
   public shippingProfileForm = new FormGroup({
     shippingCountry: new FormControl('', [Validators.required]),
@@ -104,9 +114,12 @@ export class ProfilePageComponent {
     ]),
   });
 
+  public firstName: string = '';
+  public lastName: string = '';
   public activeMainButton: string = 'general';
   public isEditMode: boolean = false;
   public isShippingDefaultChecked: boolean = false;
+  public isBillingDefaultChecked: boolean = false;
 
   public generalInputFields = generalInputFields;
   public generalInputFieldBirthDate = generalInputFieldBirthDate;
@@ -117,6 +130,7 @@ export class ProfilePageComponent {
   public passwordForDeleting = passwordForDeleting;
 
   public isFocused: Record<string, boolean> = {};
+  public isInputNotEmpty: Record<string, boolean> = {};
   public hasError = hasError;
 
   public get passwordErrorCount(): number {
@@ -127,22 +141,142 @@ export class ProfilePageComponent {
   public setActiveMainButton(buttonName: string): void {
     if (this.activeMainButton !== buttonName) {
       this.isEditMode = false;
+      this.disableAllForms();
     }
     this.activeMainButton = buttonName;
   }
 
   public setActiveEditMode(): void {
     this.isEditMode = true;
+    this.enableAllForms();
   }
 
   public setInactiveEditMode(): void {
     this.isEditMode = false;
+    this.disableAllForms();
   }
 
-  public onCheckboxChange(event: Event): void {
+  public onShippingCheckboxChange(event: Event): void {
     const target = event.target;
     if (target instanceof HTMLInputElement) {
       this.isShippingDefaultChecked = target.checked;
     }
+  }
+
+  public onBillingCheckboxChange(event: Event): void {
+    const target = event.target;
+    if (target instanceof HTMLInputElement) {
+      this.isBillingDefaultChecked = target.checked;
+    }
+  }
+
+  public onInputBlur(form: FormGroup, inputTrack: string): void {
+    const input = form.get(inputTrack);
+    this.isInputNotEmpty[inputTrack] = !!input?.value;
+    this.isFocused[inputTrack] = false;
+  }
+
+  public disableAllForms(): void {
+    this.generalProfileForm.disable();
+    this.shippingProfileForm.disable();
+    this.billingProfileForm.disable();
+  }
+
+  public enableAllForms(): void {
+    this.generalProfileForm.enable();
+    this.shippingProfileForm.enable();
+    this.billingProfileForm.enable();
+  }
+
+  public async updateGreeting(
+    firstName: string,
+    lastName: string,
+  ): Promise<void> {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  public async cancelGeneralFormChanges(): Promise<void> {
+    this.setInactiveEditMode();
+    this.setGeneralInputsValues();
+  }
+
+  public async submitGeneralFormChanges(event: Event): Promise<void> {
+    event.preventDefault();
+    this.generalProfileForm.updateValueAndValidity({
+      onlySelf: false,
+      emitEvent: true,
+    });
+    if (this.generalProfileForm.invalid) {
+      this.generalProfileForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.generalProfileForm.value);
+
+    const generalForm = this.generalProfileForm.value;
+
+    console.log(generalForm.email);
+
+    if (
+      generalForm.email &&
+      generalForm.firstName &&
+      generalForm.lastName &&
+      generalForm.birthDate
+    ) {
+      const customer_id = LocalStorageService.getCustomerId();
+      await ProfileService.changeCustomerEmail(customer_id, generalForm.email);
+      await ProfileService.changeCustomerFirstName(
+        customer_id,
+        generalForm.firstName,
+      );
+      await ProfileService.changeCustomerLastName(
+        customer_id,
+        generalForm.lastName,
+      );
+      await ProfileService.changeCustomerBirthDate(
+        customer_id,
+        generalForm.birthDate,
+      );
+      await this.updateGreeting(generalForm.firstName, generalForm.lastName);
+    }
+
+    this.setInactiveEditMode();
+  }
+
+  public async setGeneralInputsValues(): Promise<void> {
+    const customerData = await ProfileService.getCustomerDataById(
+      LocalStorageService.getCustomerId(),
+    );
+    console.log(customerData);
+
+    this.isInputNotEmpty['email'] = true;
+    this.isInputNotEmpty['firstName'] = true;
+    this.isInputNotEmpty['lastName'] = true;
+    this.isInputNotEmpty['birthDate'] = true;
+
+    if (
+      customerData?.email &&
+      customerData?.firstName &&
+      customerData?.lastName &&
+      customerData?.dateOfBirth
+    ) {
+      await this.updateGreeting(
+        customerData?.firstName,
+        customerData?.lastName,
+      );
+      this.generalProfileForm.get('email')?.setValue(customerData.email);
+      this.generalProfileForm
+        .get('firstName')
+        ?.setValue(customerData.firstName);
+      this.generalProfileForm.get('lastName')?.setValue(customerData.lastName);
+      this.generalProfileForm
+        .get('birthDate')
+        ?.setValue(customerData.dateOfBirth);
+    }
+  }
+
+  public ngOnInit(): void {
+    this.disableAllForms();
+    this.setGeneralInputsValues();
   }
 }
---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,206 @@
+import { Injectable } from '@angular/core';
+// import { ApiService } from './api.service';
+import { Customer } from '../utils/interfaces';
+import { api_url, project_key } from './confidential-data';
+import { LocalStorageService } from './local-storage.service';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+  constructor() {}
+
+  //
+  // General Profile Field methods
+  //
+  public static async getCustomerDataById(
+    customer_id: string,
+  ): Promise<Customer | null> {
+    const customer_access_token: string =
+      LocalStorageService.getCustomerAccessToken();
+
+    try {
+      const response = await fetch(
+        `${api_url}/${project_key}/customers/${customer_id}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${customer_access_token}`,
+          },
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const customer_data = await response.json();
+
+      return customer_data;
+    } catch (error) {
+      console.error('Error load custumer id:', error);
+      return null;
+    }
+  }
+
+  public static async changeCustomerEmail(
+    customer_id: string,
+    customer_new_email: string,
+  ): Promise<void> {
+    const actual_customer_version: number =
+      await ApiService.getCustomerVersion(customer_id);
+    const customer_access_token: string =
+      LocalStorageService.getCustomerAccessToken();
+
+    const fetch_body = {
+      version: actual_customer_version,
+      actions: [
+        {
+          action: 'changeEmail',
+          email: `${customer_new_email}`,
+        },
+      ],
+    };
+
+    try {
+      const response = await fetch(
+        `${api_url}/${project_key}/customers/${customer_id}`,
+        {
+          method: 'POST',
+          body: JSON.stringify(fetch_body),
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${customer_access_token}`,
+          },
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error('Changing customer email ERROR:', error);
+    }
+  }
+
+  public static async changeCustomerFirstName(
+    customer_id: string,
+    customer_new_first_name: string,
+  ): Promise<void> {
+    const actual_customer_version: number =
+      await ApiService.getCustomerVersion(customer_id);
+    const customer_access_token: string =
+      LocalStorageService.getCustomerAccessToken();
+
+    const fetch_body = {
+      version: actual_customer_version,
+      actions: [
+        {
+          action: 'setFirstName',
+          firstName: `${customer_new_first_name}`,
+        },
+      ],
+    };
+
+    try {
+      const response = await fetch(
+        `${api_url}/${project_key}/customers/${customer_id}`,
+        {
+          method: 'POST',
+          body: JSON.stringify(fetch_body),
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${customer_access_token}`,
+          },
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error('Changing customer email ERROR:', error);
+    }
+  }
+
+  public static async changeCustomerLastName(
+    customer_id: string,
+    customer_new_last_name: string,
+  ): Promise<void> {
+    const actual_customer_version: number =
+      await ApiService.getCustomerVersion(customer_id);
+    const customer_access_token: string =
+      LocalStorageService.getCustomerAccessToken();
+
+    const fetch_body = {
+      version: actual_customer_version,
+      actions: [
+        {
+          action: 'setLastName',
+          lastName: `${customer_new_last_name}`,
+        },
+      ],
+    };
+
+    try {
+      const response = await fetch(
+        `${api_url}/${project_key}/customers/${customer_id}`,
+        {
+          method: 'POST',
+          body: JSON.stringify(fetch_body),
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${customer_access_token}`,
+          },
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error('Changing customer email ERROR:', error);
+    }
+  }
+
+  public static async changeCustomerBirthDate(
+    customer_id: string,
+    customer_new_birth_date: string,
+  ): Promise<void> {
+    const actual_customer_version: number =
+      await ApiService.getCustomerVersion(customer_id);
+    const customer_access_token: string =
+      LocalStorageService.getCustomerAccessToken();
+
+    const fetch_body = {
+      version: actual_customer_version,
+      actions: [
+        {
+          action: 'setDateOfBirth',
+          dateOfBirth: `${customer_new_birth_date}`,
+        },
+      ],
+    };
+
+    try {
+      const response = await fetch(
+        `${api_url}/${project_key}/customers/${customer_id}`,
+        {
+          method: 'POST',
+          body: JSON.stringify(fetch_body),
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${customer_access_token}`,
+          },
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error('Changing customer email ERROR:', error);
+    }
+  }
+}
