24fff58a8c67d9b41c4816b997524639f52ac927
Sankovich Ilya <ilyhasankovich@gmail.com>
2025-05-31 16:15:50 +0300

    feat: add transition to product page

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { GetMinProduct } from '../../pages/catalog-page/catalog-page.component';
 import { NgClass, NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
-  imports: [NgIf, NgClass],
+  imports: [NgIf, NgClass, RouterLink],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
